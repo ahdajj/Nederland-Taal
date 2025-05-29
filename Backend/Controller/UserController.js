0bf82333bbf1801a24e2c7518a7390d13c949612
Ahmad <ahd.ajjawe@gmail.com>
2025-05-29 16:56:25 +0200

    signupNocookies

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -1,12 +1,29 @@
 const Usermodel = require('../Model/Usermodel')
-
+const bcrypt = require('bcrypt')
 
 const Signup =(req,res)=>{
-    console.log(req.body)
-    // const newuser=new Usermodel(req.body)
-    // newuser.save()
-    // .then(()=>res.json('saved'))
-    // .catch(err=>console.log(err))
+   
+  
+
+    if( !req.body.FullName || !req.body.Email || !req.body.Password){
+      return  res.json('Please fill all fields')
+    }
+    if( req.body.Email.includes('@')===false){
+       return res.json('WrongEmail')
+    }
+    Usermodel.findOne({Email:req.body.Email})
+       .then((data)=>{
+           if(data){
+            return res.json('User Exist')
+           } else {
+            var hash = bcrypt.hashSync(req.body.Password,12)
+            const newuser=new Usermodel({...req.body,Password:hash})
+             newuser.save()
+            .then(()=>res.json('saved'))
+            .catch(err=>console.log(err))
+           }
+        })
+       .catch(err =>console.log(err))  
 }
 
 
